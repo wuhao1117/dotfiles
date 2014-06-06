1c4c0d32b8832f499e655fc40901c62a94c18298
Grant Titus <grant@granttitus.com>
2014-06-06 16:46:43 -0700

    Update vimrc.d

--- vimrc.d
+++ vimrc.d
@@ -4,47 +4,56 @@ call pathogen#runtime_append_all_bundles()
 call pathogen#helptags() " generate helptags for everything in 'runtimepath'
 filetype plugin indent on
 
-set nocompatible
-set wildmenu
-set ruler
-set noswapfile
-set hlsearch
-set cursorline
-set laststatus=2
-set encoding=utf-8
-set t_Co=16
-set fillchars+=stl:\ ,stlnc:\
-set tabstop=4
-set shiftwidth=4
-set expandtab
-set backspace=2
-set wildignore+=*/tmp/*,*.so,*.swp,*.zip,node_modules
-set rtp+=~/.config/powerline/bindings/vim
-set background=dark
-
-" json synatx for vim
-autocmd BufNewFile,BufRead *.json set ft=javascript
-
-let g:airline#extensions#tabline#enabled = 1
-let g:airline_powerline_fonts = 1
-let g:airline_theme = 'solarized'
+syntax on
 colorscheme solarized
 
-syntax on
-let g:Powerline_symbols = 'unicode'
+set incsearch    "find the next match as we type the search
+set hlsearch     "highlight searches by default
+set nocompatible " be iMproved
+set wildmenu     " enhanced command-line completion
+set t_Co=16      " number of colors
+set ruler        " show line + column number of the cursor position
+set noswapfile   " no swapfiles
+set cursorline   " highlight cursor line
+set backspace=2  " backspace over everything
+set laststatus=2 " always show last status line
+set tabstop=4    " number of spaces that equal <Tab>
+set shiftwidth=4 " how many columns to shift on << or >>
+set expandtab    " insert spaces for tab
+
+set encoding=utf-8
+set background=dark 
+
+set rtp+=~/.config/powerline/bindings/vim " runtime path
+set wildignore+=*/tmp/*,*.so,*.swp,*.zip,node_modules " files to ignore
+set fillchars+=stl:\ ,stlnc:\ " set blank statusline separator
+
 noremap <C-e> :NERDTreeToggle<CR>
 noremap <F5> :%s/\s\+$//gc
 noremap <F6> :let &background = ( &background == "dark"? "light" : "dark" )<CR>
 nnoremap <C-i> :IndentGuidesToggle<CR>
 vnoremap <C-r> "hy:%s/<C-r>h//gc<left><left><left>
 
-autocmd vimenter * NERDTree
-let NERDTreeShowHidden = 1
-let g:NERDTreeWinSize = 40
-let NERDTreeIgnore = ['.git$','node_modules','.DS_Store', '\.pyc$']
+" json synatx for vim
+autocmd BufNewFile,BufRead *.json set ft=javascript
 
-set runtimepath^=~/.vim/bundle/ctrlp.vim
+" Airline
+let g:airline#extensions#tabline#enabled = 1
+let g:airline_powerline_fonts = 1
+let g:airline_theme = 'solarized'
+
+" Powerline
+let g:Powerline_symbols = 'unicode'
+
+" NERDTree
+autocmd vimenter * NERDTree
+let g:NERDTreeShowHidden = 1
+let g:NERDTreeWinSize = 40
+let g:NERDTreeIgnore = ['.git$','node_modules','.DS_Store', '\.pyc$']
+
+" CTRLP
 let g:ctrlp_map = '<c-p>'
 let g:ctrlp_cmd = 'CtrlP'
 let g:ctrlp_dont_split = 'NERD_tree_2'
-let g:ctrlp_custom_ignore = 'node_modules\|DS_Store\|docs\|git'
+let g:ctrlp_custom_ignore = 'node_modules\|DS_Store\|public\|git'
+set runtimepath^=~/.vim/bundle/ctrlp.vim
