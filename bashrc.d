95bf6f477d4636e505c4d0eb93960bd2dc0e6539
Grant Titus <grant@granttitus.com>
2014-06-26 21:18:56 -0700

    Update bashrc.d

--- bashrc.d
+++ bashrc.d
@@ -1,11 +1,36 @@
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+export VM_DEVELOP=true
+
+alias ll='ls -alF'
+alias l='ls -CF'
+
+# tmux
+alias go='cd ~/tmux-profile && ruby ./load-tmux-profile.rb dev'
+
+# navigation
+alias dev='cd ~/Quid/repos'
+alias qw='dev && cd quid/pmt'
+alias sq='dev && cd cookbooks/service_quidweb'
+alias sc='dev && cd tools/screener'
+
+# VM
+alias vbr='sudo /Library/StartupItems/VirtualBox/VirtualBox restart'
+
+vmdo () { args="$@" ; cd ~/Quid/repos/cookbooks/service_quidweb ; vagrant ssh quidweb -c "sudo /sbin/runuser -l quidweb -c 'cd /quid/apps/quidweb/current ; $args ; ' ; " ; }
+vmtest () { cd ~/Quid/repos/cookbooks/service_quidweb ; vagrant ssh quidweb -c "sudo /sbin/runuser -l quidweb -c 'cd /quid/apps/quidweb/current ; bundle exec teaspoon && bundle exec rspec; ' ; " ; }
+
 # bash completion
 if [ -f `brew --prefix`/etc/bash_completion ]; then
     . `brew --prefix`/etc/bash_completion
 fi
 
-alias __git_ps1="git branch 2>/dev/null | grep '*' | sed 's/* \(.*\)/(\1)/'"
+# git
+alias s='git status'
+alias d='git diff'
+alias gall='git add -A && git commit -m'
+alias gdm="git branch --merged | grep -v '\*' | xargs -n 1 git branch -d"
 
-# enable git PS1 prompt
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWUNTRACKEDFILES=true
 
@@ -14,26 +39,4 @@ parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1]/'
 }
 
-PS1="\[\e[0;35m\]\u@laptop\[\e[0;0m\] \w\[\e[0;34m\]\$(parse_git_branch)\[\e[0;0m\]: "
- 
-# colors and aliases in file browsing
-export CLICOLOR=1
-export LSCOLORS=ExFxCxDxBxegedabagacad
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-
-alias go="cd ~/tmux-profile && ruby ./load-tmux-profile.rb dev"
-alias dev="cd ~/Quid/repos"
-alias quidweb="dev && cd quid/pmt"
-alias qw="dev && cd quid/pmt"
-alias tools="dev && cd tools"
-alias opens="open -a 'Sublime Text'"
-alias vbr="sudo /Library/StartupItems/VirtualBox/VirtualBox restart"
-alias gdm="git branch --merged | grep -v '\*' | xargs -n 1 git branch -d"
-alias gall="git add -A && git commit -m"
-
-vmdo () { args="$@" ; cd ~/Quid/repos/cookbooks/service_quidweb ; vagrant ssh quidweb -c "sudo /sbin/runuser -l quidweb -c 'cd /quid/apps/quidweb/current ; $args ; ' ; " ; }
-vmtest () { cd ~/Quid/repos/cookbooks/service_quidweb ; vagrant ssh quidweb -c "sudo /sbin/runuser -l quidweb -c 'cd /quid/apps/quidweb/current ; bundle exec teaspoon && bundle exec rspec; ' ; " ; }
-
-export VM_DEVELOP=true
+PS1='\[\e[0;35m\]\u\[\e[0;0m\] \w\[\e[0;34m\]\$(parse_git_branch)\[\e[0;0m\]: '
